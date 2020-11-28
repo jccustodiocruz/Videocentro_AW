55ce379ff78d49151642f77bf9d3239460b92f23
Juan Carlos Custodio <juan.custodio204075@potros.itson.edu.mx>
2020-11-28 10:33:57 -0700

    Tablas

--- Videocentro/src/java/servlets/servletClientes.java
+++ Videocentro/src/java/servlets/servletClientes.java
@@ -17,19 +17,7 @@ public class servletClientes extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet servletClientes</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet servletClientes at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
     }
 
     @Override
